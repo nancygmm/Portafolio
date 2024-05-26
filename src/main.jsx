d25d2360ba898203f49bf14fca2d6278045dc68b
Nancy Mazariegos <nancymazariegos03@gmail.com>
2024-05-25 23:17:21 -0600

    Final changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./Inicio";
-import Proyectos from "./Proyectos";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Inicio/>}/>
-      <Route path="/projects" element={<Proyectos/>}/>
     </Routes>
     </BrowserRouter>
   </React.StrictMode>
